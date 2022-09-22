714f25ddcc9516bd2d03a827ee82ca854ba8f7a1
Sarr <contact@amadousarr.com>
2022-09-22 16:19:43 +0200

    Creating a Command Object and Type Coinversions in Spring MVC in order to preocess Form posts

--- src/main/java/com/amadousarr/converters/UnitOfMeasureCommandToUnitOfMeasure.java
+++ src/main/java/com/amadousarr/converters/UnitOfMeasureCommandToUnitOfMeasure.java
@@ -5,4 +5,13 @@ import com.amadousarr.domain.UnitOfMeasure;
 import org.springframework.core.convert.converter.Converter;
 
 public class UnitOfMeasureCommandToUnitOfMeasure implements Converter<UnitOfMeasureCommand, UnitOfMeasure> {
+    @Override
+    public UnitOfMeasure convert(UnitOfMeasureCommand source) {
+        return null;
+    }
+
+    @Override
+    public <U> Converter<UnitOfMeasureCommand, U> andThen(Converter<? super UnitOfMeasure, ? extends U> after) {
+        return Converter.super.andThen(after);
+    }
 }
